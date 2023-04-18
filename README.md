b03479292f5fca0344310f210fad5bca7f008a90
Etcher <96310503+etcherfx@users.noreply.github.com>
2023-04-17 20:44:20 -0700

    chore: Initial commit
    Signed-off-by: Etcher <96310503+etcherfx@users.noreply.github.com>

---
+++ README.md
@@ -0,0 +1,44 @@
+<div align ="center">
+
+<img src="projectInfo/icon.png" width="180">
+
+# Phosphur
+
+<span style="font-size:18px;">A minimalistic web proxy.</span>
+
+[![CodeFactor](https://www.codefactor.io/repository/github/etcherfx/phosphur/badge/main?style=for-the-badge)](https://www.codefactor.io/repository/github/etcherfx/phosphur/overview/main)
+[![License](https://img.shields.io/github/license/etcherfx/Phosphur?style=for-the-badge)](https://github.com/etcherfx/Phosphur/blob/main/LICENSE)
+[![GitHub issues](https://img.shields.io/github/issues/etcherfx/Phosphur?style=for-the-badge)](https://github.com/etcherfx/Phosphur/issues) <br>
+[![GitHub Release](https://img.shields.io/github/release/etcherfx/Phosphur?include_prereleases&style=for-the-badge)](https://github.com/etcherfx/Phosphur/releases/latest)
+
+</div>
+
+## Links
+
+[Website](https://phosphur.etcher.me) • [Releases](https://github.com/etcherfx/Phosphur/releases)
+
+## Projects Used
+
+- [Ultraviolet-App](https://github.com/titaniumnetwork-dev/Ultraviolet-App)
+- [Ultraviolet-Static](https://github.com/titaniumnetwork-dev/Ultraviolet-Static)
+- [acrylic-mica-css](https://github.com/yell0wsuit/acrylic-mica-css)
+
+## Deployment
+
+- Clone [Phosphur](https://github.com/etcherfx/Phosphur) to a hosting service
+- Install dependencies:
+
+  ```
+  npm install
+  ```
+
+- Start the app:
+
+  ```
+  npm start
+  ```
+
+## Credits
+
+- Icon from [icons8](https://icons8.com/icon/SA3MF2iUwDMR/measuring-cylinder)
+- Wallpapers from Microsoft's Windows 11 OS
---
+++ src/index.js
@@ -0,0 +1,77 @@
+import createBareServer from "@tomphttp/bare-server-node";
+import express from "express";
+import { createServer } from "node:http";
+import { uvPath } from "@titaniumnetwork-dev/ultraviolet";
+import { join } from "node:path";
+import { hostname } from "node:os";
+import path from 'path';
+
+const __filename = path.resolve(new URL(import.meta.url).pathname.replace(/^\/([a-z]):\//i, '$1:/'));
+const __dirname = path.dirname(__filename);
+const publicPath = path.resolve(__dirname, '../public/');
+const bare = createBareServer("/bare/");
+const app = express();
+
+// Load our publicPath first and prioritize it over UV.
+app.use(express.static(publicPath));
+// Load vendor files last.
+// The vendor's uv.config.js won't conflict with our uv.config.js inside the publicPath directory.
+app.use("/uv/", express.static(uvPath));
+
+// Error for everything else
+app.use((req, res) => {
+  res.status(404);
+  res.sendFile(join(publicPath, "404.html"));
+});
+
+const server = createServer();
+
+server.on("request", (req, res) => {
+  if (bare.shouldRoute(req)) {
+    bare.routeRequest(req, res);
+  } else {
+    app(req, res);
+  }
+});
+
+server.on("upgrade", (req, socket, head) => {
+  if (bare.shouldRoute(req)) {
+    bare.routeUpgrade(req, socket, head);
+  } else {
+    socket.end();
+  }
+});
+
+let port = parseInt(process.env.PORT || "");
+
+if (isNaN(port)) port = 8080;
+
+server.on("listening", () => {
+  const address = server.address();
+
+  // by default we are listening on 0.0.0.0 (every interface)
+  // we just need to list a few
+  console.log("Listening on:");
+  console.log(`\thttp://localhost:${address.port}`);
+  console.log(`\thttp://${hostname()}:${address.port}`);
+  console.log(
+    `\thttp://${
+      address.family === "IPv6" ? `[${address.address}]` : address.address
+    }:${address.port}`
+  );
+});
+
+process.on("SIGINT", shutdown);
+process.on("SIGTERM", shutdown);
+
+function shutdown() {
+  console.log("SIGTERM signal received: closing HTTP server");
+  server.close(() => {
+    console.log("Server closed.");
+    process.exit(0);
+  });
+}
+
+server.listen({
+  port,
+});
