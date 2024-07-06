27085e18e58f1bc9630a866046556312c376bb08
EtcherFX <96310503+etcherfx@users.noreply.github.com>
2024-07-05 22:56:29 -0700

    chore: Bump dependencies
    Signed-off-by: Etcher <96310503+etcherfx@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import { createServer } from "node:http";
 import { uvPath } from "@titaniumnetwork-dev/ultraviolet";
 import { epoxyPath } from "@mercuryworkshop/epoxy-transport";
-import { baremuxPath } from "@mercuryworkshop/bare-mux";
+import { baremuxPath } from "@mercuryworkshop/bare-mux/node";
 import { join } from "node:path";
 import { hostname } from "node:os";
 import path from 'path';
